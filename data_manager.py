7ee1f4412cfb8f8d19f60ec0797260f1b855891e
Jeff Nasson <jeffrey.nasson@gmail.com>
2026-04-20 14:05:39 -0600

    Added data manager to handle test type determinations

---
+++ data_manager.py
@@ -0,0 +1,18 @@
+import random
+from user_generator import create_positive_test_user, create_negative_test_user
+
+def resolve_credentials(case):
+    if case["type"] == "positive":
+        return create_positive_test_user()
+    elif case["type"] == "negative": 
+        return create_negative_test_user()
+    elif case["type"] == "edge":
+        return {
+            "username": "",
+            "password": ""
+        }
+    else:
+        return {
+            "username": "",
+            "password": ""
+        }
