c2909e705f3a608dff12c67adb1a9b1824adada6
Jeff Nasson <jeffrey.nasson@gmail.com>
2026-04-20 14:06:34 -0600

    updated executor with data_manager logic

--- test_executor.py
+++ test_executor.py
@@ -1,12 +1,7 @@
 from playwright.sync_api import sync_playwright
 from assertion_runner import run_real_assertion
+from data_manager import resolve_credentials
 
-# Simulate API create user function. In an enterprise application we want to do this via internal API and send credentials securely
-def create_test_user():
-    return {
-        "username": "tomsmith",
-        "password": "SuperSecretPassword!"
-    }
 
 def execute_tests(test_cases):
     results = []
@@ -31,14 +26,9 @@ def execute_tests(test_cases):
                 print(f"Assertion: {case['assertion']}\n")
                 
                 # Set inputs based on test case type
-                inputs = case.get("inputs", {})
-                if case['type'] == "positive": 
-                    user = create_test_user()
-                    username = user["username"]
-                    password = user["password"]
-                else:
-                    username = inputs.get("username","")
-                    password = inputs.get("password","")
+                creds = resolve_credentials(case) # This function determines the appropriate credentials to use based on the test case type (positive, negative, edge) and returns them in a standardized format. This allows the test execution code to be more flexible and decoupled from the specifics of how credentials are generated or stored.
+                username = creds["username"]
+                password = creds["password"]
 
                 page.fill("#username", username)
                 page.fill("#password", password)
