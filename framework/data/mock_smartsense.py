0cd7b4c41f9651b8e1a0d8225271eea3aa70fb44
Jeff Nasson <jeffrey.nasson@gmail.com>
2026-06-19 16:40:57 -0600

    added mock smartsense data

---
+++ framework/data/mock_smartsense.py
@@ -0,0 +1,11 @@
+VALID_TEMPERATURE_EVENT = {
+    "deviceId": "sensor-001",
+    "temperature": 72,
+    "timestamp": "2026-06-19T12:00:00Z"
+}
+
+
+MISSING_DEVICE_ID_EVENT = {
+    "temperature": 72,
+    "timestamp": "2026-06-19T12:00:00Z"
+}
